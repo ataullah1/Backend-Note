71f6279e2a97767447892b30f8e548b9fe39170e
Md Ataullah <ataullahm100@gmail.com>
2024-04-21 18:09:41 +0600

    Database post data

--- practices-4/sever side/index.js
+++ practices-4/sever side/index.js
@@ -25,6 +25,17 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const database = client.db('users2DB');
+    const userCollection = database.collection('users');
+
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      console.log('New User Data', user);
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db('admin').command({ ping: 1 });
     console.log(
@@ -32,7 +43,7 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
