a09185b1f4b86991aa0889cd8be5bafc9489351e
Md Ataullah <ataullahm100@gmail.com>
2024-04-22 14:11:52 +0600

    .env File added And Github sevret data remove

--- practices-4/sever side/index.js
+++ practices-4/sever side/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 200;
 
@@ -8,9 +9,7 @@ const port = process.env.PORT || 200;
 app.use(cors());
 app.use(express.json());
 
-// pass:  iCBBD3Zou7fjhCKw
-const uri =
-  'mongodb+srv://ataullah2:iCBBD3Zou7fjhCKw@cluster0.htex290.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.htex290.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -39,6 +38,14 @@ async function run() {
       const user = req.body;
       console.log('New User Data', user);
       const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+
+    app.delete('/users/:id', async (req, res) => {
+      const id = req.params.id;
+      console.log('Please delete id', id);
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
       res.send(result);
     });
 
