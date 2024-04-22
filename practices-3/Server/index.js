9ef402fbee417130a762854d8d4e689608064131
Md Ataullah <ataullahm100@gmail.com>
2024-04-22 14:06:38 +0600

    Env PASS USER ADDED

--- practices-3/Server/index.js
+++ practices-3/Server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -10,8 +11,7 @@ app.use(express.json());
 
 // MongoDB
 
-const uri =
-  'mongodb+srv://ataullah:Ataullah000@cluster0.htex290.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.htex290.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
